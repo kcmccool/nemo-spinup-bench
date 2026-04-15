c7f1645c13ada3941793d225516e12ac6b5b507c
Matt Archer <ma595@cam.ac.uk>
2026-04-15 19:36:57 +0100

    fix step 1, step 3 will error until forecast patch

--- pipeline/1-download-data.sh
+++ pipeline/1-download-data.sh
@@ -5,11 +5,11 @@ DATA_DIR="data"
 ZIP_FILE="50.zip"
 ZENODO_URL="https://zenodo.org/records/19557419/files/${ZIP_FILE}"
 
-mkdir -p "${DATA_DIR}"
+mkdir -p "${DATA_DIR}/50/"
 
 echo "Downloading DINO 50-year reference data..."
 curl -L -o "${ZIP_FILE}" "${ZENODO_URL}"
-unzip -o "${ZIP_FILE}" -d "${DATA_DIR}"
+unzip -o "${ZIP_FILE}" -d "${DATA_DIR}/50"
 rm "${ZIP_FILE}"
 
 echo "Done. Data downloaded to ${DATA_DIR}/50/"
